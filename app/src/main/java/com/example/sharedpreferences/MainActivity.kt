27648bde5d580a244b934d92aa6038de7bbcd010
Denisseortizu <denisseortizu@gmail.com>
2021-11-09 18:54:59 -0600

    Segundo commit

--- app/src/main/java/com/example/sharedpreferences/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreferences/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.sharedpreferences
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,6 +11,7 @@ import android.widget.TextView
 import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("ResourceType")
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +35,28 @@ class MainActivity : AppCompatActivity() {
         sw2.isChecked = st2
         sw3.isChecked = st3
         sw4.isChecked = st4
+
+        val sharedPref = getPreferences(Context.MODE_PRIVATE)
+
+        if (sharedPref != null){
+            val success = sharedPref.edit().putInt(getString(R.integer.saved_high_score), 60).commit()
+            if (success){
+                val newHighScore = sharedPref.getInt(getString(R.integer.saved_high_score), 5)
+                Log.d("SAVEDVALUES", "Initial commited $newHighScore")
+            }
+            sharedPref.edit().putString(getString(R.string.title_text), "Sobreescrito").commit()
+            Log.d("SAVEDVALUES","Un nuevo titulo para $titleFromPrefs")
+            val newTitleText = sharedPref.getString(getString(R.string.title_text),"Un nuevo titulo")
+        }
+
+        //val  sharedPref = getPreferences(Context.MODE_PRIVATE) ?: return
+        //with (sharedPref.edit()){
+        //       putInt(getString(R.integer.save_high_score), 60)
+        //       apply()
+        // }
+
+        sharedPref.edit().putString("algun_string_asi_nomas", "a ver si se guarda").commit()
+        Log.d("SAVEDVALUES","Guardamos el valor : ${sharedPref.getString(getString(R.string.title_text), "")}")
 
     }
 }
